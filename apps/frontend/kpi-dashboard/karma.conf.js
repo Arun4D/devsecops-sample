b2cb5678f39f3fefd326377d58a694e203b77d3d
arun <arun4.duraisamy@gmail.com>
2025-03-18 23:50:32 +0530

    change ng version

--- apps/frontend/kpi-dashboard/karma.conf.js
+++ apps/frontend/kpi-dashboard/karma.conf.js
@@ -1,20 +1,33 @@
 module.exports = function (config) {
-    config.set({
-      frameworks: ['jasmine', '@angular-devkit/build-angular'], // Ensure both Jasmine and Angular are included
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-coverage'),
-        require('@angular-devkit/build-angular/plugins/karma')
-      ],
-      reporters: ['progress', 'coverage'],
-      browsers: ['ChromeHeadless'],
-      singleRun: true,
-      coverageReporter: {
-        type: 'lcov',
-        dir: 'coverage/',
-        subdir: '.'
-      }
-    });
-  };
-  
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // Keep test results visible in the browser
+    },
+    coverageReporter: {
+      type: 'lcov',
+      dir: require('path').join(__dirname, 'coverage'),
+      subdir: '.'
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    restartOnFileChange: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: true, // Ensures tests exit after running
+    autoWatch: false, // Prevents watching files (avoids hanging)
+    captureTimeout: 60000, // Stops tests if browser doesn't connect in 60s
+    browserDisconnectTimeout: 10000, // If a browser disconnects, wait 10s before retrying
+    browserNoActivityTimeout: 30000, // Stop tests if no activity for 30s
+  });
+};
+
