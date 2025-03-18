48c00e312e693cc24ab65feae5a4a6e3aa49fb71
arun <arun4.duraisamy@gmail.com>
2025-03-19 01:30:10 +0530

    fixed coverage

--- apps/frontend/kpi-dashboard/karma.conf.js
+++ apps/frontend/kpi-dashboard/karma.conf.js
@@ -1,6 +1,5 @@
 module.exports = function (config) {
   config.set({
-    basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
@@ -9,25 +8,31 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // Keep test results visible in the browser
-    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
     coverageReporter: {
-      type: 'lcov',
       dir: require('path').join(__dirname, 'coverage'),
-      subdir: '.'
+      subdir: '.',
+      reporters: [
+        { type: 'lcovonly', subdir: '.', file: 'lcov.info' },
+        { type: 'text-summary' },
+        { type: 'html', subdir: 'html' }
+      ]
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    restartOnFileChange: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: true, // Ensures tests exit after running
-    autoWatch: false, // Prevents watching files (avoids hanging)
-    captureTimeout: 60000, // Stops tests if browser doesn't connect in 60s
-    browserDisconnectTimeout: 10000, // If a browser disconnects, wait 10s before retrying
-    browserNoActivityTimeout: 30000, // Stop tests if no activity for 30s
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--user-data-dir=/tmp/chrome-test-profile'
+        ]
+      }
+    },
+    browsers: ['ChromeHeadlessNoSandbox'],
+    singleRun: true,
+    restartOnFileChange: true
   });
-};
 
+  console.log('Karma configuration loaded');
+  console.log('Coverage reporter configured:', config.coverageReporter);
+};
