0c941b4c55beeed782f3648589092efe447bcbdb
arun <arun4.duraisamy@gmail.com>
2025-03-21 01:11:16 +0530

    added checkfix

--- ci/terraform/env/dev/main.tf
+++ ci/terraform/env/dev/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_linux_virtual_machine" "kpiVisualization" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key =  var.ssh_public_key != "" ? var.ssh_public_key : (fileexists("~/.ssh/id_rsa.pub") ? file("~/.ssh/id_rsa.pub") : null)
+    public_key =  var.ssh_public_key
   }
 
   os_disk {
